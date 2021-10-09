d82b875c4ea347c185ba3601f8326ec35ee50a61
aleinadd <aleinadhuanca@gmail.com>
2021-10-09 18:02:56 -0400

    Primer commit

---
+++ src/main/java/com/emergentes/modelo/Persona.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.emergentes.modelo;
+
+
+public class Persona {
+  private int id;
+    private String descripcion;
+    private int cantidad;
+    private int precio;
+
+    public Persona() {
+        this.id = 0;
+        this.descripcion = "";
+        this.cantidad = 0;
+        this.precio = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }  
+}
